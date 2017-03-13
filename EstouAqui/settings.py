ae358ff6456638a59fe953fbc9063a46ae794037
diego.delmiro <di3g0d0ming05@gmail.com>
2017-03-13 12:15:28 -0300

    Ajuste nao json

--- EstouAqui/settings.py
+++ EstouAqui/settings.py
@@ -66,14 +66,21 @@ WSGI_APPLICATION = 'EstouAqui.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'EstouAquiDB',
-        'USER': 'Django',
-        'PASSWORD': '261216',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'EstouAquiDB',
+#         'USER': 'Django',
+#         'PASSWORD': '261216',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 TEMPLATES = [
     {
